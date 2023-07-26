4aa9bb562e511faaa27468188ab8a70b15fd009f
Dalo Barahama <das.barahama@gmail.com>
2023-07-26 15:11:41 +0700

    delete unused code

--- app/src/main/java/com/app/keuanganku/ui/common/dialog/addsalary/DialogAddSalaryViewMvc.kt
+++ app/src/main/java/com/app/keuanganku/ui/common/dialog/addsalary/DialogAddSalaryViewMvc.kt
@@ -5,10 +5,6 @@ import com.app.keuanganku.ui.common.dialog.basedialog.BaseCustomDialogViewMvc
 
 interface DialogAddSalaryViewMvc : BaseCustomDialogViewMvc {
 
-    interface Listener {
-
-    }
-
     fun setEditTextSalary(salary: String)
     fun setSalaryEntity(salaryEntity: SalaryEntity)
 
